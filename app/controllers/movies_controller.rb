26a6d5102e22c9c5f9975c1e269ab35d9f020c23
jbunny13 <jnguyen72086@gmail.com>
2015-11-12 23:41:17 -0700

    HW 2 Part 2 Complete - Revised

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,8 @@
 class MoviesController < ApplicationController
 
   def show
-    id                  = params[:id] # retrieve movie ID from URI route
-    @movie              = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    id = params[:id]
+    @movie = Movie.find(id)
   end
 
   def index
@@ -18,8 +17,8 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie              = Movie.create!(movie_params)
-    flash[:notice]      = "#{@movie.title} was successfully created."
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
     
     redirect_to movies_path
 
@@ -32,21 +31,21 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie              = Movie.find params[:id]
+    @movie = Movie.find params[:id]
   end
 
   def update
-    @movie              = Movie.find params[:id]
+    @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
-    flash[:notice]      = "#{@movie.title} was successfully updated."
+    flash[:notice] = "#{@movie.title} was successfully updated."
     
     redirect_to movie_path(@movie)
   end
 
   def destroy
-    @movie              = Movie.find(params[:id])
+    @movie = Movie.find(params[:id])
     @movie.destroy
-    flash[:notice]      = "Movie '#{@movie.title}' deleted."
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
 
